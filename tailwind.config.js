8d1366a1031ce089c61aba64f4db1d3505d16c62
Nata Inditama <natainditama@gmail.com>
2023-05-09 07:58:43 +0800

    style: Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,34 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors')
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './src/**/*.{js,ts,jsx,tsx}'
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ["var(--font-body)", ...defaultTheme.fontFamily.sans],
+    },
+    colors: {
+      primary: colors.blue,
+      dark: colors.neutral[900],
+      light: colors.neutral[50],
+      ...colors,
+    },
     container: {
       center: true,
       padding: {
-        DEFAULT: '1rem'
-      }
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
     },
-    extend: {
-      fontFamily: {
-        sans: ['var(--font-inter)', ...defaultTheme.fontFamily.sans]
-      }
-    }
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
