e0ea35a046a0e8dca95460fa2b766e17218e2efa
natainditama <natainditama.dev@gmail.com>
2023-11-23 20:41:22 +0800

    fix: Refactored env configurations

---
+++ env.mjs
@@ -0,0 +1,8 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  client: {
+    NEXT_PUBLIC_SITE_URL: z.string().min(1),
+  },
+});
---
+++ lint-staged.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  "*.{js,jsx,ts,tsx}": ["eslint"],
+  "*.{json,yaml}": ["prettier --write"],
+};
